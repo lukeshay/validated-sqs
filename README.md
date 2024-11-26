9c82563f5f47f41654a3973113f71cda2dc5968e
Luke Shay <shay.luke17@gmail.com>
2024-11-26 12:01:59 -0600

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Validated SQS
+
+Validate your SQS messages with JSON Schema.
+
+## Usage
+
+1. Create a file with the following code:
+
+```typescript
+import { createHandler } from "validated-sqs";
+
+export const handler = createHandler({
+  schema: {
+    type: "object",
+    properties: {
+      foo: { type: "string" },
+    },
+    required: ["foo"],
+  },
+  queueUrl: "https://sqs.eu-west-1.amazonaws.com/123456789012/my-queue",
+});
+```
+
+1. Deploy it to AWS Lambda.
+
+## Why this approach?
+
+Distributing this as a package allows for maximum flexibility and the ability to be integrated into existing apps that use tools like aws-cdk and sst. This also allows for callers to add custom code before and after the validation step.
--- src/index.ts
+++ src/index.ts
@@ -1,135 +1,138 @@
-import { Ajv, type JSONSchemaType, type Schema } from "ajv";
-import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import { Ajv, type ErrorObject, type JSONSchemaType, type Schema } from "ajv";
 
 const sqs = new SQSClient();
 
 const ajv = new Ajv();
 
 const validateRequest = ajv.compile({
-  type: "object",
-  required: ["MessageBody"],
-  properties: {
-    MessageBody: {
-      type: "string",
-      description: "The content of the message.",
-    },
-    DelaySeconds: {
-      type: "integer",
-      description: "The delay in seconds for the message.",
-    },
-    MessageAttributes: {
-      type: "object",
-      additionalProperties: {
-        type: "object",
-        required: ["DataType"],
-        properties: {
-          StringValue: {
-            type: "string",
-          },
-          BinaryValue: {
-            type: "string",
-            contentEncoding: "base64",
-            description: "Binary data in base64 encoding.",
-          },
-          StringListValues: {
-            type: "array",
-            items: {
-              type: "string",
-            },
-          },
-          BinaryListValues: {
-            type: "array",
-            items: {
-              type: "string",
-              contentEncoding: "base64",
-            },
-          },
-          DataType: {
-            type: "string",
-            description: "The data type of the message attribute.",
-          },
-        },
-      },
-    },
-    MessageSystemAttributes: {
-      type: "object",
-      additionalProperties: {
-        type: "object",
-        required: ["DataType"],
-        properties: {
-          StringValue: {
-            type: "string",
-          },
-          BinaryValue: {
-            type: "string",
-            contentEncoding: "base64",
-            description: "Binary data in base64 encoding.",
-          },
-          StringListValues: {
-            type: "array",
-            items: {
-              type: "string",
-            },
-          },
-          BinaryListValues: {
-            type: "array",
-            items: {
-              type: "string",
-              contentEncoding: "base64",
-            },
-          },
-          DataType: {
-            type: "string",
-            description: "The data type of the system attribute.",
-          },
-        },
-      },
-    },
-    MessageDeduplicationId: {
-      type: "string",
-      description: "The deduplication ID for the message.",
-    },
-    MessageGroupId: {
-      type: "string",
-      description: "The group ID for the message.",
-    },
-  },
-  additionalProperties: false,
+	type: "object",
+	required: ["MessageBody"],
+	properties: {
+		MessageBody: {
+			type: "string",
+			description: "The content of the message.",
+		},
+		DelaySeconds: {
+			type: "integer",
+			description: "The delay in seconds for the message.",
+		},
+		MessageAttributes: {
+			type: "object",
+			additionalProperties: {
+				type: "object",
+				required: ["DataType"],
+				properties: {
+					StringValue: {
+						type: "string",
+					},
+					BinaryValue: {
+						type: "string",
+						contentEncoding: "base64",
+						description: "Binary data in base64 encoding.",
+					},
+					StringListValues: {
+						type: "array",
+						items: {
+							type: "string",
+						},
+					},
+					BinaryListValues: {
+						type: "array",
+						items: {
+							type: "string",
+							contentEncoding: "base64",
+						},
+					},
+					DataType: {
+						type: "string",
+						description: "The data type of the message attribute.",
+					},
+				},
+			},
+		},
+		MessageSystemAttributes: {
+			type: "object",
+			additionalProperties: {
+				type: "object",
+				required: ["DataType"],
+				properties: {
+					StringValue: {
+						type: "string",
+					},
+					BinaryValue: {
+						type: "string",
+						contentEncoding: "base64",
+						description: "Binary data in base64 encoding.",
+					},
+					StringListValues: {
+						type: "array",
+						items: {
+							type: "string",
+						},
+					},
+					BinaryListValues: {
+						type: "array",
+						items: {
+							type: "string",
+							contentEncoding: "base64",
+						},
+					},
+					DataType: {
+						type: "string",
+						description: "The data type of the system attribute.",
+					},
+				},
+			},
+		},
+		MessageDeduplicationId: {
+			type: "string",
+			description: "The deduplication ID for the message.",
+		},
+		MessageGroupId: {
+			type: "string",
+			description: "The group ID for the message.",
+		},
+	},
+	additionalProperties: false,
 });
 
 export class AjvError extends Error {
-  constructor(
-    message: string,
-    public errors: any,
-  ) {
-    super(message);
-  }
+	constructor(
+		message: string,
+		public errors:
+			| ErrorObject<string, Record<string, any>, unknown>[]
+			| null
+			| undefined,
+	) {
+		super(message);
+	}
 }
 
 export type CreateHandlerInput = {
-  schema: Schema | JSONSchemaType<any>;
-  queueUrl: string;
+	schema: Schema | JSONSchemaType<unknown>;
+	queueUrl: string;
 };
 
 export function createHandler(input: CreateHandlerInput) {
-  const validate = ajv.compile(input.schema);
+	const validate = ajv.compile(input.schema);
 
-  return async (event: any) => {
-    if (!validateRequest(event)) {
-      throw new AjvError("Invalid request", validateRequest.errors);
-    }
+	return async (event: unknown) => {
+		if (!validateRequest(event)) {
+			throw new AjvError("Invalid request", validateRequest.errors);
+		}
 
-    if (!validate(JSON.parse(event.MessageBody))) {
-      throw new AjvError("Invalid message body", validate.errors);
-    }
+		if (!validate(JSON.parse(event.MessageBody))) {
+			throw new AjvError("Invalid message body", validate.errors);
+		}
 
-    const result = await sqs.send(
-      new SendMessageCommand({
-        ...event,
-        QueueUrl: input.queueUrl,
-      }),
-    );
+		const result = await sqs.send(
+			new SendMessageCommand({
+				...event,
+				QueueUrl: input.queueUrl,
+			}),
+		);
 
-    return result;
-  };
+		return result;
+	};
 }
